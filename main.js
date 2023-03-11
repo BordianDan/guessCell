212b8da687b09e297fe121350bb1d98adf59374f
BordianDan <dan.bordian@gmail.com>
2023-03-11 18:34:25 +0200

    Code optimization

--- main.js
+++ main.js
@@ -1,83 +1,92 @@
+const htmlElements = {
+  body: document.querySelector('body'),
+  wrapper: document.querySelector('#table'),
+  header: document.createElement('header'),
+  table: document.querySelector('.table'),
+  script: document.querySelector('script'),
+  title: document.createElement('h1'),
+  instruction: document.createElement('p'),
+  button: document.createElement('button'),
+  cells: document.querySelectorAll('td'),
+  timer: document.createElement('p')
+}
+
+const values = {
+  Id: 1,
+  time: 0,
+  randomNumbersSet: new Set(),
+  correctCells: new Set(),
+  randomNumbersArray: [],
+  
+}
+
 document.body.style.backgroundColor = 'gray'
-const body = document.querySelector('body')
-const header = document.createElement('header')
-const table = document.querySelector('.table')
-const script = document.querySelector('script')
-body.insertBefore(header, table)
-const title = document.createElement('h1')
-header.appendChild(title)
-title.innerText = 'Guess Cell'
-const instruction = document.createElement('p')
-header.appendChild(instruction)
-instruction.innerText = 'Guess 7 cells in 30sec! Beware, one cell is deadly!'
-instruction.style.fontWeight = 'bold'
-const button = document.createElement('button')
-body.insertBefore(button, script)
-button.addEventListener('click', play)
-button.innerText = 'Play'
-const cells = document.querySelectorAll('td')
-cells.forEach(i => i.style.width = '30px')
-cells.forEach(i => i.style.height = '30px')
-cells.forEach(i => i.style.border = '1px solid black')
-let Id = 1
-cells.forEach(e => e.setAttribute('id', `${Id++}`))
-const timer = document.createElement('p')
-header.appendChild(timer)
-timer.innerText = 'Timer: 30 sec'
-let time
-const randomNumbersSet = new Set()
-let randomNumbersArray
+htmlElements.body.insertBefore(htmlElements.header, htmlElements.table)
+htmlElements.header.appendChild(htmlElements.title)
+htmlElements.title.innerText = 'Guess Cell'
+htmlElements.header.appendChild(htmlElements.instruction)
+htmlElements.instruction.innerText = 'Guess 7 cells in 30sec! Beware, one cell is deadly!'
+htmlElements.instruction.style.fontWeight = 'bold'
+htmlElements.body.insertBefore(htmlElements.button, htmlElements.script)
+htmlElements.button.addEventListener('click', play)
+htmlElements.button.innerText = 'Play'
+htmlElements.cells.forEach(i => i.style.width = '30px')
+htmlElements.cells.forEach(i => i.style.height = '30px')
+htmlElements.cells.forEach(i => i.style.border = '1px solid black')
+htmlElements.cells.forEach(e => e.setAttribute('id', `${values.Id++}`))
+htmlElements.header.appendChild(htmlElements.timer)
+htmlElements.timer.innerText = 'Timer: 30 sec'
+
 let intervalTimer
-let correctCells = new Set()
 
 function play() {
-  cells.forEach(e => e.addEventListener('click', guess))
-  cells.forEach(e => e.style.backgroundColor = 'white')
-  time = 30
+  htmlElements.cells.forEach(e => e.addEventListener('click', guess))
+  htmlElements.cells.forEach(e => e.style.backgroundColor = 'white')
+  values.time = 30
   countDown()
   intervalTimer = setInterval(countDown, 1000)
   generateRandomNumbers()
 }
 
 function generateRandomNumbers() {
-  while(randomNumbersSet.size < 11) {
-    randomNumbersSet.add(Math.floor(Math.random()* 100) + 1)
+  while(values.randomNumbersSet.size < 11) {
+    values.randomNumbersSet.add(Math.floor(Math.random()* 100) + 1)
   }
-    randomNumbersArray = Array.from(randomNumbersSet)
-    console.log(randomNumbersArray);
+  values.randomNumbersArray = Array.from(values.randomNumbersSet)
+    console.log(values.randomNumbersArray);
  }
 
  function countDown() {
-  time === 0 ? gameOver() : timer.innerText = `Timer: ${time--} sec`
+  values.time === 0 ? gameOver() : htmlElements.timer.innerText = `Timer: ${values.time--} sec`
 }
  
 function guess(e) {
-if (randomNumbersArray[0] == e.srcElement.id) {
+if (values.randomNumbersArray[0] == e.srcElement.id) {
   e.srcElement.style.backgroundColor = 'black'
   return gameOver()
- } else if (randomNumbersArray.find(elem => elem == e.srcElement.id)) {  
-    correctCells.add(e.srcElement.id)
+ } else if (values.randomNumbersArray.find(elem => elem == e.srcElement.id)) {  
+  values.correctCells.add(e.srcElement.id)
     e.srcElement.style.backgroundColor = 'green'
-    console.log(correctCells);
+    console.log(values.correctCells);
  } else e.srcElement.style.backgroundColor = 'brown'
   
  winAlert()
 }
 
 function winAlert() {
- if(correctCells.size === 7) {
+ if(values.correctCells.size === 7) {
   gameOver()
-  instruction.innerText = 'You WIN!!!'
+  htmlElements.instruction.innerText = 'You WIN!!!'
  }
 }
 
 function gameOver() {
   clearInterval(intervalTimer)
-  cells.forEach(e => e.removeEventListener('click', guess))
-  time === 0 ? instruction.innerText = 'TIME OUT!!!' : 
-  instruction.innerText = 'GAME OVER!'
-  timer.innerText = `Timer: 0 sec`
-  randomNumbersArray.length = 0
-  randomNumbersSet.clear()
-  correctCells.clear()
+  htmlElements.cells.forEach(e => e.removeEventListener('click', guess))
+  values.time === 0 ? instruction.innerText = 'TIME OUT!!!' : 
+  htmlElements.instruction.innerText = 'GAME OVER!'
+  htmlElements.timer.innerText = `Timer: 0 sec`
+  values.randomNumbersArray.length = 0
+  values.randomNumbersSet.clear()
+  values.correctCells.clear()
 }
